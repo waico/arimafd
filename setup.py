01ec180ae521e9d5ca376cd3c27b24b10b861ffb
Vyacheslav <rfptk2525@yandex.ru>
2020-02-06 22:04:50 +0300

    0.20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name='arimafd',
-      version='0.19',
+      version='0.20',
       url='https://github.com/waico/arimafd',
       license='MIT',
       #packages=find_packages(),
